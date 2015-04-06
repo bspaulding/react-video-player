14f110b184bae7526e586c6cb323fc5bf98951df
Bradley J. Spaulding <brad.spaulding@gmail.com>
2015-04-06 13:59:05 -0700

    Cleared up clutter for build

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-var React = require("react");
-var VideoPlayer = require("./components/video-player.jsx");
-
-var sources = [{ src: "video.mp4", type: "video/mp4" }];
-var style = {
-  maxWidth: "640px",
-  margin: "0px auto"
-};
-React.render(<VideoPlayer poster="poster.png" sources={sources} style={style}/>, document.body);
