5e751e9155aa418d09751bae7a198d07001e3820
Bradley J. Spaulding <brad.spaulding@gmail.com>
2015-04-06 13:56:24 -0700

    Reverting to original video

--- dist/main.js
+++ dist/main.js
@@ -22009,7 +22009,7 @@ module.exports = {
 var React = require("react");
 var VideoPlayer = require("./components/video-player.jsx");
 
-var sources = [{ src: "video-h264.mp4", type: "video/mp4" }];
+var sources = [{ src: "video.mp4", type: "video/mp4" }];
 var style = {
   maxWidth: "640px",
   margin: "0px auto"
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 var React = require("react");
 var VideoPlayer = require("./components/video-player.jsx");
 
-var sources = [{ src: "video-h264.mp4", type: "video/mp4" }];
+var sources = [{ src: "video.mp4", type: "video/mp4" }];
 var style = {
   maxWidth: "640px",
   margin: "0px auto"
